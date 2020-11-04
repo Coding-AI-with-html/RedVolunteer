66813607cf9c1a2dd9c5dbbd7f063a8dc7b795c0
Coding-AI-with-html <mankuspijus@gmail.com>
2020-11-05 00:07:00 +0200

    Changing build.gradle

--- app/src/main/java/LoginAndRegister/Login.java
+++ app/src/main/java/LoginAndRegister/Login.java
@@ -3,4 +3,6 @@ package LoginAndRegister;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+
+
 }
--- app/src/main/java/com/redvolunteer/MainActivity.java
+++ app/src/main/java/com/redvolunteer/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseDatabase FireDatabase;
+    DatabaseReference DataRefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupFirebaseAuth();
+    }
+
+
+    public void setupFirebaseAuth(){
+        mAuth = mAuth.getInstance();
+
     }
 }
