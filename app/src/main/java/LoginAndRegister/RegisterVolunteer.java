01d7a8451c9790e2355881efe837100ca108cea1
Coding-AI-with-html <mankuspijus@gmail.com>
2020-11-04 23:47:31 +0200

    Making more layout files, setuping firebase, preparing buttons, text fields

--- app/src/main/java/LoginAndRegister/RegisterVolunteer.java
+++ app/src/main/java/LoginAndRegister/RegisterVolunteer.java
@@ -1,8 +1,17 @@
 package LoginAndRegister;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.redvolunteer.R;
 
 public class RegisterVolunteer extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_volunteer);
+    }
 }
--- app/src/main/java/LoginAndRegister/RegisterX.java
+++ app/src/main/java/LoginAndRegister/RegisterX.java
@@ -1,8 +1,19 @@
 package LoginAndRegister;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.redvolunteer.R;
+
 public class RegisterX extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    setContentView(R.layout.register_helpseeker);
+    }
 
 
 }
