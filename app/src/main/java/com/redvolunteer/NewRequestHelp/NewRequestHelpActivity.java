a26125fe96eab95200408a4016a43c444899ebd1
Coding-AI-with-html <mankuspijus@gmail.com>
2020-11-20 17:14:19 +0200

    Error with maps, navigation turned on, but Say's its not, also changed MainActivity with button click, need to change in on RequestWallFragment

--- app/src/main/java/com/redvolunteer/NewRequestHelp/NewRequestHelpActivity.java
+++ app/src/main/java/com/redvolunteer/NewRequestHelp/NewRequestHelpActivity.java
@@ -1,5 +1,7 @@
 package com.redvolunteer.NewRequestHelp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -18,6 +20,7 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.common.api.ApiException;
@@ -35,7 +38,6 @@ import com.redvolunteer.pojo.RequestLocation;
 import com.redvolunteer.pojo.User;
 import com.google.android.libraries.places.api.Places;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
@@ -43,6 +45,8 @@ import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class NewRequestHelpActivity extends AppCompatActivity  {
     private static final String TAG = "NewRequestHelpActivity";
+
+    private Context mContext = NewRequestHelpActivity.this;
 
     /**
      * Request being craited
@@ -136,25 +140,31 @@ public class NewRequestHelpActivity extends AppCompatActivity  {
 
 
                 Places.initialize(getApplicationContext(), "AIzaSyAAxujBxhopVBhW9Eb3hh-3lSKLgtLaXk0");
-                PlacesClient ClientPlace = Places.createClient(NewRequestHelpActivity.this);
+                PlacesClient ClientPlace = Places.createClient(mContext);
 
 
                 /**
                  * Takes User current location and makes it HelpRequestLocation
                  */
-                List<Place.Field> placeField = Arrays.asList(Place.Field.NAME);
+                List<Place.Field> placeField = Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG);
 
                 FindCurrentPlaceRequest requestLocation = FindCurrentPlaceRequest.builder(placeField).build();
 
-                if (ContextCompat.checkSelfPermission(NewRequestHelpActivity.this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions((Activity) mContext, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+                }
                     ClientPlace.findCurrentPlace(requestLocation).addOnSuccessListener(((response) -> {
                         for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
-
+                            Log.d(TAG, "onClick: Navigation working");
                             mRetrievedLocation = new RequestLocation(placeLikelihood.getPlace().getLatLng().latitude, placeLikelihood.getPlace().getLatLng().longitude);
                             mRetrievedLocation.setName(placeLikelihood.getPlace().getAddress().toString());
                             mRetrievedPositionLabel.setText(placeLikelihood.getPlace().getAddress().toString());
 
                             positionSelected = true;
+                            Log.d(TAG, "onClick: " + placeLikelihood.getPlace().getLatLng().latitude +  placeLikelihood.getPlace().getLatLng().longitude);
 
                         }
 
@@ -164,11 +174,6 @@ public class NewRequestHelpActivity extends AppCompatActivity  {
                             Log.e(TAG, "Place not found: " + apiException.getStatusCode());
                         }
                     });
-                } else {
-
-                    Toast.makeText(NewRequestHelpActivity.this, "Reikia ijungti navigacija", Toast.LENGTH_SHORT).show();
-                }
-
 
             }
         });
