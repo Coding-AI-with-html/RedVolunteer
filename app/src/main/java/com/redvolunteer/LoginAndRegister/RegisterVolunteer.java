261c24de6dbd8b330431b833fe18dc74247d0adc
Coding-AI-with-html <mankuspijus@gmail.com>
2020-12-01 22:59:05 +0200

    Register Activity

--- app/src/main/java/com/redvolunteer/LoginAndRegister/RegisterVolunteer.java
+++ app/src/main/java/com/redvolunteer/LoginAndRegister/RegisterVolunteer.java
@@ -1,18 +1,35 @@
 package com.redvolunteer.LoginAndRegister;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.redvolunteer.MainActivity;
 import com.redvolunteer.R;
+import com.redvolunteer.pojo.User;
 import com.redvolunteer.utils.ValidateUtils;
 import com.redvolunteer.viewmodels.UserViewModel;
+
+import io.reactivex.functions.Consumer;
 
 public class RegisterVolunteer extends AppCompatActivity {
 
@@ -63,6 +80,72 @@ public class RegisterVolunteer extends AppCompatActivity {
 
     private void bindFacebookButton(){
 
-        fbRegister = (Button) findViewById(R.id.register_button);
+        fbRegister = (Button) findViewById(R.id.facebook_register_btn);
+
+
+        LoginManager.getInstance().registerCallback(fbCallBackManager, new FacebookRegisterRequestCallBack());
+    }
+
+    private class FacebookRegisterRequestCallBack implements FacebookCallback<LoginResult> {
+
+        @Override
+        public void onSuccess(LoginResult registerResult) {
+            showWhaitSpinner();
+            handleFacebookAccesTokenForFirebase(registerResult.getAccessToken());
+        }
+
+        @Override
+        public void onCancel() {
+            stopSpinner();
+            Toast.makeText(getApplicationContext(), R.string.facebook_cancel_string, Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onError(FacebookException error) {
+            stopSpinner();
+            Toast.makeText(getApplicationContext(), R.string.facebook_login_error_string, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void handleFacebookAccesTokenForFirebase(AccessToken token){
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        buildFirebaseUser(credential);
+    }
+
+    private void buildFirebaseUser(final AuthCredential credential){
+
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+
+                            mUserViewModel.retireveUSerFromRemoteStore().subscribe(new Consumer<User>() {
+                                @Override
+                                public void accept(User user) throws Exception {
+
+                                    startMainActivity();
+                                }
+                            });
+                        } else {
+                            stopSpinner();
+                            Toast.makeText(getApplicationContext(), R.string.facebook_error_credential_string, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
+    private void startMainActivity(){
+
+        stopSpinner();
+
+        if(this.mUserViewModel.isAuth()){
+            startActivity(new Intent(this, MainActivity.class));
+
+        } else {
+            Toast.makeText(this, "E-pasto klaida", Toast.LENGTH_LONG).show();
+        }
     }
 }
